06849126bf9590a17e285d5f1a4e5e4dc95e4f44
tnagler <thomas.nagler@tum.de>
2016-12-23 15:03:12 +0100

    use only bicop.hpp to include all families and the class (#6 13)

--- src/common/include/bicop.hpp
+++ src/common/include/bicop.hpp
@@ -1,31 +1,69 @@
 /*
-    Copyright 2016 Thibault Vatter, Thomas Nagler
+Copyright 2016 Thibault Vatter, Thomas Nagler
 
-    This file is part of vinecopulib.
+This file is part of vinecopulib.
 
-    vinecopulib is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+vinecopulib is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    vinecopulib is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+vinecopulib is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with vinecopulib.  If not, see <http://www.gnu.org/licenses/>.
- */
+You should have received a copy of the GNU General Public License
+along with vinecopulib.  If not, see "http://www.gnu.org/licenses/".
+*/
 
-#ifndef VINECOPULIB_BICOP_HPP
-#define VINECOPULIB_BICOP_HPP
-    // include the class and all families
+#ifndef VINECOPULIB_BICOP_FAMILIES_HPP
+#define VINECOPULIB_BICOP_FAMILIES_HPP
+
+    // include all familie headers not included yet (alphabetic order);
+    // this automatically includes the bicop_class header
+    #ifndef VINECOPULIB_BICOP_ARCHIMEDEAN_HPP
+    #include "bicop_archimedean.hpp"
+    #endif
+
     #ifndef VINECOPULIB_BICOP_CLASS_HPP
     #include "bicop_class.hpp"
     #endif
 
-    #ifndef VINECOPULIB_BICOP_FAMILIES_HPP
-    #include "bicop_families.hpp"
+    #ifndef VINECOPULIB_BICOP_CLAYTON_HPP
+    #include "bicop_clayton.hpp"
+    #endif
+
+    #ifndef VINECOPULIB_BICOP_ELLIPTICAL_HPP
+    #include "bicop_elliptical.hpp"
+    #endif
+
+    #ifndef VINECOPULIB_BICOP_FRANK_HPP
+    #include "bicop_frank.hpp"
+    #endif
+
+    #ifndef VINECOPULIB_BICOP_GAUSS_HPP
+    #include "bicop_gauss.hpp"
+    #endif
+
+    #ifndef VINECOPULIB_BICOP_GUMBEL_HPP
+    #include "bicop_gumbel.hpp"
+    #endif
+
+    #ifndef VINECOPULIB_BICOP_INDEP_HPP
+    #include "bicop_indep.hpp"
+    #endif
+
+    #ifndef VINECOPULIB_BICOP_JOE_HPP
+    #include "bicop_joe.hpp"
+    #endif
+
+    #ifndef VINECOPULIB_BICOP_PARAMETRIC_HPP
+    #include "bicop_parametric.hpp"
+    #endif
+
+    #ifndef VINECOPULIB_BICOP_STUDENT_HPP
+    #include "bicop_student.hpp"
     #endif
 
 #endif
