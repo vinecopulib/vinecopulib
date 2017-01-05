5049925debfdf71601020ac3a170d5402819ee6a
tnagler <thomas.nagler@tum.de>
2017-01-05 04:54:10 +0100

    port interpolation functionality from kdecopula: class InterpolationGrid

---
+++ include/bicop_interpolation.hpp
@@ -0,0 +1,57 @@
+/*
+Copyright 2016 Thibault Vatter, Thomas Nagler
+
+This file is part of vinecopulib.
+
+vinecopulib is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+vinecopulib is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with vinecopulib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef VINECOPULIB_BICOP_KERNEL_HPP
+#define VINECOPULIB_BICOP_KERNEL_HPP
+
+#include <Eigen/Dense>
+
+typedef Eigen::VectorXd VecXd;
+typedef Eigen::MatrixXd MatXd;
+
+//! A class for cubic spline interpolation of bivariate copulas
+//!
+//! The class is used for implementing kernel estimators. It makes storing the
+//! observations obsolete and allows for fast numerical integration.
+class InterpolationGrid {
+public:
+    InterpolationGrid(const VecXd& gridpoints, const MatXd& values);
+
+    VecXd interpolate(const MatXd& x);
+    VecXd hfunc(const MatXd& uev, const int& cond_var);
+    VecXd hinv(const MatXd& uev, const int& cond_var);
+
+private:
+    // Utility functions for spline Interpolation
+    double cubic_poly(const double& x, const VecXd& a);
+    double cubic_indef_integral(const double& x, const VecXd& a);
+    double cubic_integral(const double& lower, const double& upper, const VecXd& a);
+    double inv_cubic_integral(const double& q, const VecXd& a);
+    VecXd find_coefs(const VecXd& vals, const VecXd& grid);
+    double interp_on_grid(const double& x, const VecXd& vals, const VecXd& grid);
+
+    // Utility functions for integration
+    double int_on_grid(const double& upr, const VecXd& vals, const VecXd& grid);
+    double inv_int_on_grid(const double& qq, const VecXd& vals, const VecXd& grid);
+
+    VecXd gridpoints_;
+    MatXd values_;
+};
+
+#endif
