79066b4e19757ebe572f0c8c7cb202bbb93cc1b7
tvatter <thibault.vatter@gmail.com>
2017-01-19 11:19:42 -0500

    try the release version on travis

--- test/test_boost_tools.cpp
+++ test/test_boost_tools.cpp
@@ -17,12 +17,12 @@ You should have received a copy of the GNU General Public License
 along with vinecopulib.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "gtest/gtest.h"
 #include "include/boost_tools.hpp"
 #include <gsl/gsl_cdf.h>
 #include <gsl/gsl_randist.h>
 #include <Eigen/Dense>
-#include <ctime>
+#include <chrono>
+#include <iostream>
 
 template<typename T> T dnorm_gsl(const T& x)
 {
@@ -37,29 +37,27 @@ template<typename T> T qnorm_gsl(const T& x)
     return x.unaryExpr(std::ptr_fun(gsl_cdf_ugaussian_Pinv));
 };
 
-int main(int argc, char **argv) {
+template <typename T>
+void time(const std::string label, const T &it)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    it();
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = finish - start;
+    std::cout << label << "Elapsed time: " << elapsed.count() << " s\n";
+}
 
-    Eigen::VectorXd x = Eigen::VectorXd::Ones(1e5);
-    x = 0.5*x;
-    Eigen::VectorXd y = x;
-    clock_t begin_time = clock();
-    y = dnorm(x);
-    std::cout << float( clock () - begin_time ) /  CLOCKS_PER_SEC << std::endl;
-    begin_time = clock();
-    y = dnorm_gsl(x);
-    std::cout << float( clock () - begin_time ) /  CLOCKS_PER_SEC << std::endl;
-    begin_time = clock();
-    y = pnorm(x);
-    std::cout << float( clock () - begin_time ) /  CLOCKS_PER_SEC << std::endl;
-    begin_time = clock();
-    y = pnorm_gsl(x);
-    std::cout << float( clock () - begin_time ) /  CLOCKS_PER_SEC << std::endl;
-    begin_time = clock();
-    y = qnorm(x);
-    std::cout << float( clock () - begin_time ) /  CLOCKS_PER_SEC << std::endl;
-    begin_time = clock();
-    y = qnorm_gsl(x);
-    std::cout << float( clock () - begin_time ) /  CLOCKS_PER_SEC << std::endl;
+
+int main(int argc, char **argv) {
+    Eigen::MatrixXd m = Eigen::MatrixXd::Ones(10000, 10000);
+    m *= 0.5;
+
+    time("Boost dnorm:", [m]{ auto a = dnorm(m); });
+    time("GSL dnorm:  ", [m]{ auto b = dnorm_gsl(m); });
+    time("Boost pnom:", [m]{ auto a = pnorm(m); });
+    time("GSL pnorm:  ", [m]{ auto b = pnorm_gsl(m); });
+    time("Boost qnom:", [m]{ auto a = qnorm(m); });
+    time("GSL qnorm:  ", [m]{ auto b = qnorm_gsl(m); });
 
     return 0;
 }
