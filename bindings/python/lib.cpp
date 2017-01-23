e62138d828647f825d47b166f4604bfc4b681d6a
sarabas <sarabas@chathamfinancial.eu>
2017-01-23 08:54:25 +0000

    first additions towards getting Python bindings compiled with CMake

---
+++ bindings/python/lib.cpp
@@ -0,0 +1,15 @@
+#include <boost/python.hpp>
+
+#include <git_revision.h>
+
+BOOST_PYTHON_MODULE(pyvinecopulib)
+{
+    namespace bp = boost::python;
+
+    // specify that this module is actually a package
+    bp::object package = bp::scope();
+    package.attr("__path__") = "libcloudphxx";
+
+    // exposing git revision id
+    package.attr("git_revision") = GIT_REVISION;
+}
