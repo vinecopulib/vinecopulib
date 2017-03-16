6a2ba182b6c5763aa70288af4010af6c284d6ae3
sarabas <sarabas@chathamfinancial.eu>
2017-03-16 16:15:18 +0000

    python bindings update - vinecop class, more converters, ...

--- bindings/python/pyvinecopulib.cpp
+++ bindings/python/pyvinecopulib.cpp
@@ -1,19 +1,14 @@
 #include <boost/python.hpp>
-//#include <boost/python/numpy.hpp>
 
-#include <bicop.hpp>
-
-
-void register_converters();
+void register_eigen_converters();
 void export_git_revision();
 void export_bicop_class();
+void export_vinecop_class();
 
 BOOST_PYTHON_MODULE(pyvinecopulib)
 {
-//    Py_Initialize();
-//    boost::python::numpy::initialize();
-
-    register_converters();
+    register_eigen_converters();
     export_git_revision();
     export_bicop_class();
+    export_vinecop_class();
 }
