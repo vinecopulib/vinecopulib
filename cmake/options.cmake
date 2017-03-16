bdcd50d13e98b49150e85ee5f170e0b42168f1c3
tvatter <thibault.vatter@gmail.com>
2017-03-16 14:41:48 -0400

    revert to shared library but with static option

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,4 +1,5 @@
 set(CMAKE_EXPORT_COMPILE_COMMANDS "ON")
+SET(BUILD_SHARED_LIBS ON)
 
 option(WARNINGS_AS_ERRORS   "Compiler warnings as errors"       "OFF")
 option(OPT_ASAN             "Use adress sanitizer (debug)"      "ON")
