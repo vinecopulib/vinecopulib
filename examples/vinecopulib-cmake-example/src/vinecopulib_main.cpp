5eb8dc1af16b8da32f8563308e5da4732e555d0c
tvatter <thibault.vatter@gmail.com>
2017-03-16 18:00:37 -0400

    correct indentation in example

--- examples/vinecopulib-cmake-example/src/vinecopulib_main.cpp
+++ examples/vinecopulib-cmake-example/src/vinecopulib_main.cpp
@@ -7,23 +7,24 @@ int main() {
 
     // print all available families
 	auto family = bicop_families::all;
-    std::cout << "Available families:";
+	std::cout << "Available families:";
 	for (auto fam : family) {
 		std::cout << get_family_name(fam) << " ";
 	}
-    std::cout << "\n";
+	std::cout << "\n";
 
 	// create Clayton copula and simulate from the model
 	auto model = Bicop::create(BicopFamily::clayton, 0, Eigen::VectorXd::Constant(1,1));
     auto data = model->simulate(1e3);
-    std::cout << "Created Model | family: " << model->get_family_name() << ", " <<
-                    "rotation: " <<  model->get_rotation() << ", " <<
-                        "parameter: " << model->get_parameters() << std::endl;
+    std::cout << "Created Model | family: " << model->get_family_name() << ", ";
+	std::cout << "rotation: " <<  model->get_rotation() << ", ";
+    std::cout << "parameter: " << model->get_parameters() << std::endl;
 
 	// fit and select family
 	auto fitted = Bicop::select(data);
-	std::cout << "Fitted Model | family: " << fitted->get_family_name() << ", " <<
-                    "rotation: " <<  fitted->get_rotation() << ", " <<
-                        "parameter: " << fitted->get_parameters() << std::endl;
+	std::cout << "Created Model | family: " << fitted->get_family_name() << ", ";
+	std::cout << "rotation: " <<  fitted->get_rotation() << ", ";
+	std::cout << "parameter: " << fitted->get_parameters() << std::endl;
+
 	return 0;
 }
