7e90c349f8ff4dca348377ec718e44c33e88cae6
tvatter <thibault.vatter@gmail.com>
2017-03-16 17:52:08 -0400

    add dummy cmake project to test make install

---
+++ examples/vinecopulib-cmake-example/src/vinecopulib_main.cpp
@@ -0,0 +1,29 @@
+#include <bicop_class.hpp>
+#include <iostream>
+
+using namespace vinecopulib;
+
+int main() {
+
+    // print all available families
+	auto family = bicop_families::all;
+    std::cout << "Available families:";
+	for (auto fam : family) {
+		std::cout << get_family_name(fam) << " ";
+	}
+    std::cout << "\n";
+
+	// create Clayton copula and simulate from the model
+	auto model = Bicop::create(BicopFamily::clayton, 0, Eigen::VectorXd::Constant(1,1));
+    auto data = model->simulate(1e3);
+    std::cout << "Created Model | family: " << model->get_family_name() << ", " <<
+                    "rotation: " <<  model->get_rotation() << ", " <<
+                        "parameter: " << model->get_parameters() << std::endl;
+
+	// fit and select family
+	auto fitted = Bicop::select(data);
+	std::cout << "Fitted Model | family: " << fitted->get_family_name() << ", " <<
+                    "rotation: " <<  fitted->get_rotation() << ", " <<
+                        "parameter: " << fitted->get_parameters() << std::endl;
+	return 0;
+}
