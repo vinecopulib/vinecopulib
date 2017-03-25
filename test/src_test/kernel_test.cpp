f8825ca73233e25fe56f3080320a6fd1d6e62b5a
tnagler <thomas.nagler@tum.de>
2017-03-25 18:05:29 +0100

    rename Trafokernel -> Tll0

---
+++ test/src_test/kernel_test.cpp
@@ -0,0 +1,13 @@
+// Copyright © 2017 Thomas Nagler and Thibault Vatter
+//
+// This file is part of the vinecopulib library and licensed under the terms of
+// the MIT license. For a copy, see the LICENSE file in the root directory of
+// vinecopulib or https://tvatter.github.io/vinecopulib/.
+
+#include "include/kernel_test.hpp"
+#include "include/misc/tools_stats.hpp"
+
+TrafokernelTest::TrafokernelTest() {
+    bicop_ = Bicop(vinecopulib::BicopFamily::tll0, 0);
+    u = tools_stats::simulate_uniform(100, 2);
+}
