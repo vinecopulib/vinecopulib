0cc3a6d740cde9f80491c2057418805927fd09d5
sarabas <sarabas@chathamfinancial.eu>
2017-05-12 10:10:50 +0000

    trying to fix behaviour with GCC 6

--- cmake/findNlopt.cmake
+++ cmake/findNlopt.cmake
@@ -1,1 +1,7 @@
 find_package(NLopt REQUIRED)
+
+# this is needed for GCC 6 because we later use these as SYSTEM headers
+# see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=70129
+#     https://issues.apache.org/jira/browse/THRIFT-3828
+#     https://gitlab.kitware.com/cmake/cmake/issues/16291
+get_filename_component(NLOPT_INCLUDE_DIRS ${NLOPT_INCLUDE_DIRS} REALPATH)
