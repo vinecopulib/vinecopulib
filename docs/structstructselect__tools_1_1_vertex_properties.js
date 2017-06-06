16d318e5b17966b29e590c25b2826a3319c7af6c
tvatter <thibault.vatter@gmail.com>
2017-06-06 12:14:42 -0400

    update docs + 1

--- docs/structstructselect__tools_1_1_vertex_properties.js
+++
@@ -1,8 +0,0 @@
-var structstructselect__tools_1_1_vertex_properties =
-[
-    [ "conditioned", "structstructselect__tools_1_1_vertex_properties.html#a55e4762ff80a2764ad3108296461f1eb", null ],
-    [ "conditioning", "structstructselect__tools_1_1_vertex_properties.html#ae98731fd772dc856678430a58c89a59c", null ],
-    [ "hfunc1", "structstructselect__tools_1_1_vertex_properties.html#a02eb01c5bac207cda84e1bb774af31e0", null ],
-    [ "hfunc2", "structstructselect__tools_1_1_vertex_properties.html#a4944630229999934498193b125540b72", null ],
-    [ "prev_edge_indices", "structstructselect__tools_1_1_vertex_properties.html#a4417fead3115618173f27aa82dc2a892", null ]
-];
