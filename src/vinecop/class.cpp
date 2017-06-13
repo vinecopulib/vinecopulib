0391a25c939f6d6b1d4892b832209321604d65ed
tvatter <thibault.vatter@gmail.com>
2017-06-13 10:34:15 -0400

    try debugging on windows +4

--- src/vinecop/class.cpp
+++ src/vinecop/class.cpp
@@ -101,7 +101,6 @@ namespace vinecopulib
     //!      a valid R-vine matrix.
     Vinecop::Vinecop(boost::property_tree::ptree input, bool check_matrix) {
 
-        typedef boost::property_tree::ptree pt;
         boost::property_tree::write_json(std::cout, input);
         auto matrix = tools_serialization::ptree_to_matrix<size_t>(
                 input.get_child("matrix"));
@@ -109,13 +108,15 @@ namespace vinecopulib
         d_ = (size_t) matrix.rows();
 
         pair_copulas_ = make_pair_copula_store(d_);
-        pt pcs_node = input.get_child("pair copulas");
+        boost::property_tree::ptree pcs_node = input.get_child("pair copulas");
         for (size_t tree = 0; tree < d_ - 1; ++tree) {
             std::cout << "tree" + std::to_string(tree) << std::endl;
-            pt tree_node = pcs_node.get_child("tree" + std::to_string(tree));
+            boost::property_tree::ptree tree_node = pcs_node.get_child(
+                    "tree" + std::to_string(tree));
             for (size_t edge = 0; edge < d_ - tree - 1; ++edge) {
                 std::cout << "edge" + std::to_string(edge) << std::endl;
-                pt pc_node = tree_node.get_child("pc" + std::to_string(edge));
+                boost::property_tree::ptree pc_node = tree_node.get_child(
+                        "pc" + std::to_string(edge));
                 boost::property_tree::write_json(std::cout, pc_node);
                 pair_copulas_[tree][edge] = Bicop(pc_node);
             }
