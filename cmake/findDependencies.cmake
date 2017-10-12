837cb2392589b516e9957b167025468f568cf64a
tvatter <thibault.vatter@gmail.com>
2017-10-12 12:10:29 -0400

    fix travis + 2

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -3,11 +3,12 @@
 #set(Boost_USE_STATIC_RUNTIME OFF)
 
 include(cmake/findEigen3.cmake            REQUIRED)
+message(STATUS "Eigen3 version: ${EIGEN3_VERSION}")
 include(cmake/findR.cmake                 REQUIRED)
 find_package(Boost 1.56                   REQUIRED)
 find_package(Threads                      REQUIRED)
 
-find_package(NLopt                        QUIET)
+find_package(NLopt)
 if ( NLOPT_INCLUDE_DIRS )
     # this is needed for GCC 6 because we later use these as SYSTEM headers
     # see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=70129
@@ -16,11 +17,9 @@ if ( NLOPT_INCLUDE_DIRS )
     get_filename_component(NLOPT_INCLUDE_DIRS ${NLOPT_INCLUDE_DIRS} REALPATH)
 else ( NLOPT_INCLUDE_DIRS )
     # for NLopt versions installed with apt-get/brew
+    message(STATUS "NLopt not found via find_package, try locating it in standard repos.")
     include(cmake/findNLopt.cmake            REQUIRED)
 endif()
-
-
-
 
 set(external_includes ${EIGEN3_INCLUDE_DIR} ${NLOPT_INCLUDE_DIRS} ${Boost_INCLUDE_DIRS})
 
