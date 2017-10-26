d716a36c3007785d22ab6b1476e10bdd1674b246
tvatter <thibault.vatter@gmail.com>
2017-10-26 14:40:01 -0400

    add BUILD_SHARED_LIBS option

--- cmake/findR.cmake
+++ cmake/findR.cmake
@@ -13,7 +13,7 @@ if(BUILD_TESTING)
     set(CMAKE_FIND_APPBUNDLE ${TEMP_CMAKE_FIND_APPBUNDLE})
 
     if(RSCRIPT_EXECUTABLE)
-        configure_file(${CMAKE_CURRENT_LIST_DIR}/templates/rscript.hpp.in ${CMAKE_BINARY_DIR}/generated/rscript.hpp)
+        configure_file(${CMAKE_CURRENT_LIST_DIR}/templates/rscript.hpp.in ${CMAKE_BINARY_DIR}/generated/test/rscript.hpp)
     elseif(RSCRIPT_EXECUTABLE)
         message(SEND_ERROR "BUILD_TESTING requires Rscript to be installed in a standard location.")
     endif(RSCRIPT_EXECUTABLE)
--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,11 +1,11 @@
 set(CMAKE_EXPORT_COMPILE_COMMANDS "ON")
-set(BUILD_SHARED_LIBS ON)
 set(CMAKE_MACOSX_RPATH 1)
 
 if(CMAKE_BUILD_TYPE STREQUAL "")
     set(CMAKE_BUILD_TYPE Release)
 endif()
 
+option(BUILD_SHARED_LIBS    "Non-header only version"           "OFF")
 option(WARNINGS_AS_ERRORS   "Compiler warnings as errors"       "OFF")
 option(OPT_ASAN             "Use adress sanitizer (debug)"      "ON")
 option(BUILD_TESTING        "Build tests."                      "ON")
