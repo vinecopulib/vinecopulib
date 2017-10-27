88af1ef6bd151dc583777263c240ca03d8e26320
Thibault Vatter <thibault.vatter@gmail.com>
2017-10-26 21:42:27 -0400

    Compiled lib option (#249)
    * add BUILD_SHARED_LIBS option
    
    * update examples for BUILD_SHARED_LIBS
    
    * do not erase headers if already generated
    
    * modify CI to include BUILD_SHARED_LIBS
    
    * do not erase headers if already generated+1
    
    * modify appveyor
    
    * remove unused include in tools_interface
    
    * don't link against gmock (leads to odr-violations)
    
    * remove threads dependency
    
    * use VINECOPULIB_SHARED_LIB instead of BUILD_SHARED_LIBS
    
    * bring back pthreads

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,12 +1,12 @@
 set(CMAKE_EXPORT_COMPILE_COMMANDS "ON")
-set(BUILD_SHARED_LIBS ON)
 set(CMAKE_MACOSX_RPATH 1)
 
 if(CMAKE_BUILD_TYPE STREQUAL "")
     set(CMAKE_BUILD_TYPE Release)
 endif()
 
-option(WARNINGS_AS_ERRORS   "Compiler warnings as errors"       "OFF")
-option(OPT_ASAN             "Use adress sanitizer (debug)"      "ON")
-option(BUILD_TESTING        "Build tests."                      "ON")
-option(CODE_COVERAGE        "Code coverage."                    "OFF")
+option(VINECOPULIB_SHARED_LIB    "Pre-compiled version"              "OFF")
+option(WARNINGS_AS_ERRORS        "Compiler warnings as errors"       "OFF")
+option(OPT_ASAN                  "Use adress sanitizer (debug)"      "ON")
+option(BUILD_TESTING             "Build tests."                      "ON")
+option(CODE_COVERAGE             "Code coverage."                    "OFF")
