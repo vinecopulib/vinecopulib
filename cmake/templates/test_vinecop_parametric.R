f1aaec8697e500bb05d62cacb4d9aef6d2561065
tnagler <thomas.nagler@tum.de>
2017-12-04 22:06:16 +0100

    adapt unit tests

--- cmake/templates/test_vinecop_parametric.R
+++ cmake/templates/test_vinecop_parametric.R
@@ -14,6 +14,7 @@ mat <- matrix(c(4, 0, 0, 0, 0, 0, 0,
               7, 7, byrow = TRUE)
 fam <- par <- matrix(0, 7, 7)
 fam[lower.tri(fam)] <- 23
+fam[1:4, ] <- 0  # 3-truncated
 par[lower.tri(par)] <- -3
 model <- VineCopula::RVineMatrix(mat, fam, par)
 u <- VineCopula::RVineSim(1000, model)
