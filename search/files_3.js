5e07da2eacd119d642c48108b43df1c249d67337
tnagler <thomas.nagler@tum.de>
2018-01-04 18:38:35 +0100

    update docs to 0.2.4

---
+++ search/files_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['rvine_5fmatrix_2eipp',['rvine_matrix.ipp',['../rvine__matrix_8ipp.html',1,'']]]
+];
---
+++ search/files_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['tools_5fstats_2eipp',['tools_stats.ipp',['../tools__stats_8ipp.html',1,'']]]
+];
