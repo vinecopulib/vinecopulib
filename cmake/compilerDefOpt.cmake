fcd695e7f25f628305fd5451dbed3d5a2cab8648
Thibault Vatter <thibault.vatter@gmail.com>
2021-11-30 13:40:21 -0500

    link libtorch to vinecopulib

--- cmake/compilerDefOpt.cmake
+++ cmake/compilerDefOpt.cmake
@@ -1,7 +1,13 @@
 if(NOT WIN32)
 
+    if(TORCH_INSTALL_PREFIX)
+        set(CMAKE_CXX_FLAGS   "-std=c++14")
+    else()
+        set(CMAKE_CXX_FLAGS   "-std=gnu++11")
+    endif()
+
     if(STRICT_COMPILER AND CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-      set(CMAKE_CXX_FLAGS                "-std=gnu++11 -Werror -Wno-delete-non-virtual-dtor -Wall -Wextra -Wconversion")
+      set(CMAKE_CXX_FLAGS                "${CMAKE_CXX_FLAGS} -Werror -Wno-delete-non-virtual-dtor -Wall -Wextra -Wconversion")
         set(CMAKE_CXX_FLAGS                "${CMAKE_CXX_FLAGS} -Wstrict-aliasing -pedantic -fmax-errors=5 -Werror=return-type")
         set(CMAKE_CXX_FLAGS                "${CMAKE_CXX_FLAGS} -Wunreachable-code -Wcast-align -Wcast-qual")
         set(CMAKE_CXX_FLAGS                "${CMAKE_CXX_FLAGS} -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op")
@@ -9,7 +15,7 @@ if(NOT WIN32)
         set(CMAKE_CXX_FLAGS                "${CMAKE_CXX_FLAGS} -Wredundant-decls -Wshadow -Wsign-promo -Wstrict-null-sentinel")
         set(CMAKE_CXX_FLAGS                "${CMAKE_CXX_FLAGS} -Wno-variadic-macros -Wno-parentheses -fdiagnostics-show-option")
     else()
-        set(CMAKE_CXX_FLAGS                "-std=gnu++11 -Wextra -Wall -Wno-delete-non-virtual-dtor -Werror=return-type")
+        set(CMAKE_CXX_FLAGS                "${CMAKE_CXX_FLAGS} -Wextra -Wall -Wno-delete-non-virtual-dtor -Werror=return-type")
     endif()
 
     set(CMAKE_CXX_FLAGS_DEBUG          "-g -O0 -DDEBUG ")
@@ -42,4 +48,8 @@ endif()
 
 if (MSVC)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /bigobj")
+endif()
+
+if (TORCH_INSTALL_PREFIX)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TORCH_CXX_FLAGS}")
 endif()
--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -5,4 +5,8 @@ find_package(Boost 1.56                   REQUIRED)
 find_package(Threads                      REQUIRED)
 find_package(wdm                          REQUIRED)
 
+if(TORCH_INSTALL_PREFIX)
+  find_package(Torch REQUIRED PATHS ${TORCH_INSTALL_PREFIX})
+endif()
+
 set(external_includes ${EIGEN3_INCLUDE_DIR} ${Boost_INCLUDE_DIRS} ${wdm_INCLUDE_DIRS})
