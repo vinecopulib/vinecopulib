7cb1266b1ed04cbf344ad2fd43324d82c1d59dc5
Thibault Vatter <thibault.vatter@gmail.com>
2022-01-06 16:17:21 -0500

    use wdm dev

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -4,17 +4,32 @@ include(cmake/findR.cmake                 REQUIRED)
 find_package(Boost 1.56                   REQUIRED)
 find_package(Threads                      REQUIRED)
 
-# Find wdm and install if not found
+include(FetchContent)
+FetchContent_Declare(
+  googletest
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG        703bd9caab50b139428cea1aaff9974ebee5742e # release-1.10.0
+  URL https://github.com/google/googletest/archive/e2239ee6043f73722e7aa812a459f54a28552929.zip
+)
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_Declare(
+  wdm
+  URL https://github.com/tnagler/wdm/archive/refs/heads/dev.zip
+)
+
+# Find wdm and download if not found
+# Download googlestest
 find_package(wdm QUIET)
 if(NOT wdm_FOUND)
-  message(STATUS "BLABLA")
-  include(FetchContent)
-  FetchContent_Declare(
-    wdm
-    URL https://github.com/tnagler/wdm/archive/refs/heads/master.zip
-  )
-  FetchContent_MakeAvailable(wdm)
+  if(BUILD_TESTING)
+    FetchContent_MakeAvailable(wdm googletest)
+  else()
+    FetchContent_MakeAvailable(wdm)
+  endif()
   set(wdm_INCLUDE_DIRS "${wdm_SOURCE_DIR}/include")
+elseif(BUILD_TESTING)
+  FetchContent(googletest)
 endif()
 
 # Set all the external dependencies
