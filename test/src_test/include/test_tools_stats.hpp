2c759eb17374d1cf4f26ecb5092f14b6053639fe
tnagler <thomas.nagler@tum.de>
2023-01-19 18:22:39 +0100

    try to fix macos install

--- test/src_test/include/test_tools_stats.hpp
+++ test/src_test/include/test_tools_stats.hpp
@@ -39,7 +39,7 @@ TEST(test_tools_stats, to_pseudo_obs_is_correct)
   EXPECT_NO_THROW(tools_stats::to_pseudo_obs(X2, "first"));
   EXPECT_ANY_THROW(tools_stats::to_pseudo_obs(X2, "something"));
 
-  X2.col(0).head(50) = Eigen::VectorXd::Constant(n, NAN);
+  X2.col(0).head(50) = Eigen::VectorXd::Constant(50, NAN);
   auto u = tools_stats::to_pseudo_obs(X2);
   EXPECT_TRUE(std::isnan(u(0, 0)));
   EXPECT_GE(u.col(0).tail(50).maxCoeff(), 0.98);
