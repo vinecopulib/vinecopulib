e6cbbb3646b34aae3afb8ee0ede5cdf16816b8f0
Thibault Vatter <thibault.vatter@gmail.com>
2024-03-23 10:21:02 +0100

    try+79

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -7,13 +7,15 @@ find_package(wdm                          REQUIRED)
 
 # Download googlestest
 if(BUILD_TESTING)
-  set(gtest_force_shared_crt on)
   include(FetchContent)
   FetchContent_Declare(
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG        6b74da4757a549563d7c37c8fae3e704662a043b # release-1.10.0
   )
+
+  # For Windows: Prevent overriding the parent project's compiler/linker settings
+  set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
   FetchContent_MakeAvailable(googletest)
 endif()
 
