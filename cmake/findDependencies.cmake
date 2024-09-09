b2d80e06ad8fec3a8317e19107f08cbe5bda7575
Thibault Vatter <thibault.vatter@gmail.com>
2024-09-09 19:37:53 +0200

    Debug ASAN issues (#583)
    * Test in debug without turning off ASAN
    
    * Add UBSan
    
    * Make code coverage independant of asan
    
    * Cleanup build to replicate ASAN issue

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -7,15 +7,16 @@ find_package(wdm                          REQUIRED)
 
 # Download googlestest
 if(BUILD_TESTING)
+
+  # Prevent overriding the parent project's compiler/linker settings
+  set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
   include(FetchContent)
   FetchContent_Declare(
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG        f8d7d77c06936315286eb55f8de22cd23c188571 # release-1.14.0
   )
-
-  # For Windows: Prevent overriding the parent project's compiler/linker settings
-  set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
   # Download and configure googletest
   FetchContent_MakeAvailable(googletest)
