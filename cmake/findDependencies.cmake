46b4baaafb89542d4f2089f155efa45613a4228f
Thibault Vatter <thibault.vatter@gmail.com>
2024-12-09 14:43:51 +0100

    Make doc optional

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -55,17 +55,17 @@ endif()
 # Set all the external dependencies
 set(external_includes ${EIGEN3_INCLUDE_DIR} ${Boost_INCLUDE_DIRS} ${wdm_INCLUDE_DIRS})
 
-# Find doxygen and configure if found
-find_package(Doxygen QUIET)
-if(DOXYGEN_FOUND)
-    configure_file(
-        ${CMAKE_CURRENT_SOURCE_DIR}/docs/Doxyfile.in
-        ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY
-    )
-    add_custom_target(doc
-        ${DOXYGEN_EXECUTABLE}
-        ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-        COMMENT "Generating API documentation with Doxygen" VERBATIM
-    )
-endif (DOXYGEN_FOUND)
+if(BUILD_DOC)
+  # Find doxygen and configure if found
+  find_package(Doxygen REQUIRED)
+  configure_file(
+          ${CMAKE_CURRENT_SOURCE_DIR}/docs/Doxyfile.in
+          ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY
+      )
+  add_custom_target(doc
+          ${DOXYGEN_EXECUTABLE}
+          ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+          WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+          COMMENT "Generating API documentation with Doxygen" VERBATIM
+      )
+endif()
