6f12a8fd145e38c932a1526aa7ce189eae1dc09e
Thibault Vatter <thibault.vatter@gmail.com>
2025-06-12 14:13:36 +0200

    Properly do static code analysis

--- test/test_bicop_select.cpp
+++ test/test_bicop_select.cpp
@@ -8,8 +8,8 @@
 
 using namespace test_bicop_select;
 
-int
-main(int argc, char** argv)
+auto
+main(int argc, char** argv) -> int
 {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
