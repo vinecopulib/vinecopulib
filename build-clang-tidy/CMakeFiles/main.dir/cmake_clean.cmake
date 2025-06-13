49b8455bba8cebd8cd103222beb36fdef0a571b5
Thibault Vatter <thibault.vatter@gmail.com>
2025-06-13 11:06:23 +0200

    Reduce the number of checks

---
+++ build-clang-tidy/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/link.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ examples/minimal/main.cpp
@@ -0,0 +1,16 @@
+// Copyright © 2016-2025 Thomas Nagler and Thibault Vatter
+//
+// This file is part of the vinecopulib library and licensed under the terms of
+// the MIT license. For a copy, see the LICENSE file in the root directory of
+// vinecopulib or https://vinecopulib.github.io/vinecopulib/.
+
+#include <vinecopulib.hpp>
+
+using namespace vinecopulib;
+
+int main() {
+    // Minimal usage to force instantiation of core headers
+    auto bicop = Bicop();
+    auto vinecop = Vinecop();
+    return 0;
+}
